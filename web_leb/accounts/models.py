0f1ef29ce5bb9d18f0f967cf4778ccebd548d9d1
KimHyunSoo <wishinghyun@naver.com>
2020-06-27 13:58:24 +0900

    login page

--- web_leb/accounts/models.py
+++ web_leb/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class User(AbstractUser):
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    birth = models.CharField(max_length=6)
+    gender = models.CharField(max_length=2)
--- web_leb/accounts/views.py
+++ web_leb/accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        
