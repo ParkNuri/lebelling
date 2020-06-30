40b5f2c9a9471b2d1cda64fffb77eba541908ff9
yyyljy <yyyljy@gmail.com>
2020-06-30 23:52:00 +0900

    0630 taging, login

--- web_leb/taging/views.py
+++ web_leb/taging/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def images(request):
+    if request.method == 'POST':
+        print(request.POST.length)
     return render(request, 'taging/images.html')
